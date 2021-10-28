4d6dc52463024e82553e448dd364d5defe59924e
rostynio <rostyslav.laba@maven.one>
2021-10-28 11:26:08 +0300

    Minor changes

--- express/src/lib/oauth-promise.js
+++ express/src/lib/oauth-promise.js
@@ -2,57 +2,77 @@
 //and keep our code sexier
 
 module.exports = (oauthCallback) => {
-  
   const CONSUMER_KEY = process.env.CONSUMER_KEY;
   const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
-  const _oauth = new (require('oauth').OAuth)(
-      'https://api.twitter.com/oauth/request_token',
-      'https://api.twitter.com/oauth/access_token',
-      CONSUMER_KEY, // consumer key
-      CONSUMER_SECRET, // consumer secret
-      '1.0',
-      oauthCallback,
-      'HMAC-SHA1'
+  const _oauth = new (require("oauth").OAuth)(
+    "https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
+    "dsFdRDvQ65Ki67LAQRfUcsNGo", // consumer key
+    "NLv8Svbf1YH3vBrLjYSX1fwdL9FNIWGZNUdVYykX9QnSeNXdIT", // consumer secret
+    "1.0",
+    oauthCallback,
+    "HMAC-SHA1"
   );
-  
-  const oauth ={
-    getOAuthRequestToken: () => { 
+
+  const oauth = {
+    getOAuthRequestToken: () => {
       return new Promise((resolve, reject) => {
-        _oauth.getOAuthRequestToken((error, oauth_token, oauth_token_secret, results) => {
-          if(error) {
-            reject(error);  
-          } else {
-            resolve({oauth_token, oauth_token_secret, results});  
+        _oauth.getOAuthRequestToken(
+          (error, oauth_token, oauth_token_secret, results) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve({ oauth_token, oauth_token_secret, results });
+            }
           }
-        });
+        );
       });
     },
-    
-    getOAuthAccessToken: (oauth_token, oauth_token_secret, oauth_verifier) => { 
+
+    getOAuthAccessToken: (oauth_token, oauth_token_secret, oauth_verifier) => {
       return new Promise((resolve, reject) => {
-        _oauth.getOAuthAccessToken(oauth_token, oauth_token_secret, oauth_verifier, (error, oauth_access_token, oauth_access_token_secret, results) => {
-          if(error) {
-            reject(error);  
-          } else {
-            resolve({oauth_access_token, oauth_access_token_secret, results});  
+        _oauth.getOAuthAccessToken(
+          oauth_token,
+          oauth_token_secret,
+          oauth_verifier,
+          (error, oauth_access_token, oauth_access_token_secret, results) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve({
+                oauth_access_token,
+                oauth_access_token_secret,
+                results,
+              });
+            }
           }
-        });
+        );
       });
     },
-    
-    getProtectedResource: (url, method, oauth_access_token, oauth_access_token_secret) => {
-       return new Promise((resolve, reject) => {
-        _oauth.getProtectedResource(url, method, oauth_access_token, oauth_access_token_secret,  (error, data, response) => {
-          if(error) {
-            reject(error);  
-          } else {
-            resolve({data, response});  
+
+    getProtectedResource: (
+      url,
+      method,
+      oauth_access_token,
+      oauth_access_token_secret
+    ) => {
+      return new Promise((resolve, reject) => {
+        _oauth.getProtectedResource(
+          url,
+          method,
+          oauth_access_token,
+          oauth_access_token_secret,
+          (error, data, response) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve({ data, response });
+            }
           }
-        });
-      });   
-    }
-    
+        );
+      });
+    },
   };
-  
+
   return oauth;
-}
+};
